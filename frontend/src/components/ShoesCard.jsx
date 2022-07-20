fa5b1883b854ac338f90fc032c416ae52e781245
coral Mejia <coral.mejia99@gmail.com>
2022-07-20 13:57:28 -0400

    created shoes page & shoesCard component

---
+++ frontend/src/components/ShoesCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ShoesCard({shoesData}) {
+  return (
+    <div>
+        <h1> {shoesData.name} </h1>
+        <img src={shoesData.grid_picture_url} alt={shoesData.name} />
+        <h2> ${shoesData.retail_price} </h2>
+        
+    </div>
+  )
+}
+
+export default ShoesCard
--- frontend/src/pages/Shoes.jsx
+++ frontend/src/pages/Shoes.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import ShoesCard from '../components/ShoesCard'
 
 function Shoes() {
+
+
+const [shoes, setShoes] = useState([])
+
+const getShoesData = async () => {
+  const response = await fetch(`http://localhost:8000/api/sneakers`)
+  const data = await response.json ()
+
+  console.log('data:',data.shoes)
+  setShoes(data.shoes)
+  console.log(shoes)
+}
+
+useEffect(() => {
+  getShoesData() 
+}, [])
+
   return (
-    <div>Shoes</div>
+    <div>
+      <h1>All Shoes</h1>
+      <div>
+        {shoes.length > 0 ? shoes.map((shoe) => <ShoesCard key = {shoe.id} shoesData = {shoe} />) : 'loading...'}
+      </div>
+    </div>
+
   )
 }
 
