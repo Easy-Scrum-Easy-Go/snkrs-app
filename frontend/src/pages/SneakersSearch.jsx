76a27f6c5bf119ec5f04a077e3c6ae3d583a5b03
ShirelleW <shirellewinecoffm@gmail.com>
2022-07-21 12:58:13 -0400

    Added Search functionality

--- frontend/src/pages/SneakersSearch.jsx
+++ frontend/src/pages/SneakersSearch.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useParams } from 'react'
 import CSS from "../CSS/index.css"
-
+import {Link} from "react-router-dom"
 
 function SneakersSearch() {
 
     const [shoes, setShoes] = useState([])
+    const [query, setQuery] = useState("")
+    console.log(query)
+
 
     const getShoesData = async () => {
         const response = await fetch(`http://localhost:8000/api/sneakers`)
@@ -22,21 +25,23 @@ function SneakersSearch() {
 
     return (
         <div>
-            <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search for Sneakers..."
-                aria-label="Search"
-            // onChange={e => setQuery(e.target.value)}
-            />
-
+            <div className='search-bar-container'>
+                <input
+                    class="form-control me-2 search-bar"
+                    type="search"
+                    placeholder="Search for Sneakers..."
+                    aria-label="Search"
+                    onChange={e => setQuery(e.target.value)}
+                />
+            </div>
 
             <table className='search-table'>
                 <tbody>
                     <tr>
                         <th className='search-title'>Sneaker</th>
                     </tr>
-                    {shoes.map((shoe) => <tr>{shoe.name} <img src={shoe.grid_picture_url} alt="test"></img></tr>)}
+                    {shoes.filter(shoe => shoe.name.toLowerCase().includes(query)).map((shoe) => <tr>{shoe.name} <img src={shoe.grid_picture_url} alt="test"></img><Link to={`/sneakers/${shoe.id}`}>Go To Shoe</Link></tr>)}
+                    
                 </tbody>
             </table>
         </div>
